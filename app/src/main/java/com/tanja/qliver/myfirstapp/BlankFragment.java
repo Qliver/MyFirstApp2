6b517de29a6c6099c79cb072109fe48dba242123
Qliver <Pqlamy11>
2016-01-10 20:25:33 +0100

    Initial commit

---
+++ app/src/main/java/com/tanja/qliver/myfirstapp/BlankFragment.java
@@ -0,0 +1,16 @@
+package com.tanja.qliver.myfirstapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BlankFragment extends AppCompatActivity {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+}
